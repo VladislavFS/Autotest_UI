d83e4fe593aaa0459edd7bfdcced488ed85abf53
Work <vladfomin548@gmail.com>
2024-03-06 13:04:14 +0300

    Добавление action для запуска тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
+import sys
 
 
 @pytest.fixture(scope="module")
 def browser():
-    return webdriver.Chrome()\
-
-
+    os = sys.platform
+    if os == 'win32':
+        return webdriver.Chrome()
+    else:
+        options = Options()
+        options.add_argument('--headless')
+        return webdriver.Chrome(options=options)
